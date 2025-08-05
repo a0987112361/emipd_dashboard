761a81e1dbfeb6d412691f4878b874c0943c1aa7
dennis <a0987112361@gmail.com>
2025-08-05 09:16:35 +0800

    add password

--- src/Containers/Login/LoginScreen.js
+++ src/Containers/Login/LoginScreen.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { UserActions } from 'src/Stores';
-import { Button, Modal, Input, Form,  } from 'antd';
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { UserActions } from "src/Stores";
+import { Button, Modal, Input, Form } from "antd";
 
-import { LoginMiddleView } from 'src/Components';
-import Swal from 'sweetalert2';
+import { LoginMiddleView } from "src/Components";
+import Swal from "sweetalert2";
 
 const styles = {
   root: {
     flexGrow: 1,
-    height: '100%',
-    overflowY: 'hidden',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    padding: '30px',
+    height: "100%",
+    overflowY: "hidden",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    padding: "30px",
   },
   btnStyle: {
-    width: '100px',
-    height: '40px',
-    color: '#fff',
-    backgroundColor: '#004C7C',
-    borderRadius: '4px',
-    marginBottom: '40px'
+    width: "100px",
+    height: "40px",
+    color: "#fff",
+    backgroundColor: "#004C7C",
+    borderRadius: "4px",
+    marginBottom: "40px",
   },
 };
 
@@ -35,7 +35,7 @@ class LoginScreen extends React.Component {
       isOpen: true,
       isLoading: false,
       firstLoginUser: {},
-      firstLoginToken: '',
+      firstLoginToken: "",
       failTimes: 0,
     };
   }
@@ -44,92 +44,139 @@ class LoginScreen extends React.Component {
   };
 
   componentDidMount = () => {
-    let record = JSON.parse(localStorage.getItem('EMIPD_FAIL'));
-    if(record){
-      this.setState({ failTimes: record.count})
-      if(new Date(Date.now()).getTime() > new Date(record.ok_date).getTime()){
-        this.setState({ 
-            failTimes: 0
-          }, () => // 如果現在要登入時 已超過第五次的15分鐘時間則清掉重算
-          localStorage.removeItem("EMIPD_FAIL")
-        )
+    let record = JSON.parse(localStorage.getItem("EMIPD_FAIL"));
+    if (record) {
+      this.setState({ failTimes: record.count });
+      if (new Date(Date.now()).getTime() > new Date(record.ok_date).getTime()) {
+        this.setState(
+          {
+            failTimes: 0,
+          },
+          () =>
+            // 如果現在要登入時 已超過第五次的15分鐘時間則清掉重算
+            localStorage.removeItem("EMIPD_FAIL")
+        );
       }
-    } 
-  }
+    }
+  };
 
   handleLogin = (value) => {
     const { login } = this.props;
 
     const callback = (value, token) => {
-      this.setState({
-        isLoading: false,
-        firstLoginUser: value,
-        firstLoginToken: token
-      }, () => {
-        if(value.is_login === true) {
-          this.handleResetPasswordModal()
+      this.setState(
+        {
+          isLoading: false,
+          firstLoginUser: value,
+          firstLoginToken: token,
+        },
+        () => {
+          if (value.is_login === false) {
+            this.handleResetPasswordModal();
+          }
         }
-      });
-    }
+      );
+    };
 
     const errorCallback = (value) => {
-      if(value.data.success === false){
+      if (value.data.success === false) {
         let now = new Date(Date.now());
-        let next = new Date(new Date().getTime() + 15*60*1000); // 15min later
+        let next = new Date(new Date().getTime() + 15 * 60 * 1000); // 15min later
         let record = localStorage.getItem("EMIPD_FAIL");
-        let newRecord = {}; let oldRecord = {};
-        
-        if(record === null){  // 第1次登入錯誤
+        let newRecord = {};
+        let oldRecord = {};
+
+        if (record === null) {
+          // 第1次登入錯誤
           newRecord.login_date = now.toString();
           newRecord.login_time = new Date(Date.now()).toLocaleTimeString();
           newRecord.count = 1;
-          this.setState({ failTimes: 1 }, () => {window.localStorage.setItem("EMIPD_FAIL", JSON.stringify(newRecord));} )
-        }else{  // 登入錯誤次數1次以上
-          oldRecord = JSON.parse(localStorage.getItem('EMIPD_FAIL'));
+          this.setState({ failTimes: 1 }, () => {
+            window.localStorage.setItem(
+              "EMIPD_FAIL",
+              JSON.stringify(newRecord)
+            );
+          });
+        } else {
+          // 登入錯誤次數1次以上
+          oldRecord = JSON.parse(localStorage.getItem("EMIPD_FAIL"));
           let oldCount = JSON.parse(localStorage.getItem("EMIPD_FAIL")).count;
-          
+
           newRecord.login_date = now.toString();
           newRecord.login_time = new Date(Date.now()).toLocaleTimeString();
           newRecord.ok_date = next.toString();
           newRecord.count = oldCount + 1;
 
-
-          if(oldCount >= 4){  // 登入錯誤次數5次以上
-            if(new Date(Date.now()).getDate() === new Date(oldRecord.ok_date).getDate()){  // 如果跟之前允許登入的日期同天
+          if (oldCount >= 4) {
+            // 登入錯誤次數5次以上
+            if (
+              new Date(Date.now()).getDate() ===
+              new Date(oldRecord.ok_date).getDate()
+            ) {
+              // 如果跟之前允許登入的日期同天
               newRecord.ok_date = next.toString();
-              if(newRecord.count === 5){
+              if (newRecord.count === 5) {
                 this.setState({ failTimes: 5 }, () => {
                   Swal.fire({
-                    icon: 'error',
-                    title: '登入錯誤已達5次，將鎖定15分鐘無法登入。'
-                  })
-                  window.localStorage.setItem("EMIPD_FAIL", JSON.stringify(newRecord));
-                })
-                
-              }else{
-                if(new Date(Date.now()).getTime() > new Date(oldRecord.ok_date).getTime()){
-                  newRecord.count = 1
-                  this.setState({ failTimes: 1 }, () => {window.localStorage.setItem("EMIPD_FAIL", JSON.stringify(newRecord));} )
-                }else{
-                  this.setState({ failTimes: 5 })
+                    icon: "error",
+                    title: "登入錯誤已達5次，將鎖定15分鐘無法登入。",
+                  });
+                  window.localStorage.setItem(
+                    "EMIPD_FAIL",
+                    JSON.stringify(newRecord)
+                  );
+                });
+              } else {
+                if (
+                  new Date(Date.now()).getTime() >
+                  new Date(oldRecord.ok_date).getTime()
+                ) {
+                  newRecord.count = 1;
+                  this.setState({ failTimes: 1 }, () => {
+                    window.localStorage.setItem(
+                      "EMIPD_FAIL",
+                      JSON.stringify(newRecord)
+                    );
+                  });
+                } else {
+                  this.setState({ failTimes: 5 });
                 }
               }
-            }else{  // 如果不同天，就重新寫入全部的值、次數調為1
+            } else {
+              // 如果不同天，就重新寫入全部的值、次數調為1
               newRecord = {
                 ...oldRecord,
-                count: 1
-              }
-              this.setState({ failTimes: 1 }, () => {window.localStorage.setItem("EMIPD_FAIL", JSON.stringify(newRecord));} )
+                count: 1,
+              };
+              this.setState({ failTimes: 1 }, () => {
+                window.localStorage.setItem(
+                  "EMIPD_FAIL",
+                  JSON.stringify(newRecord)
+                );
+              });
             }
-          }else{  // 登入錯誤次數2~4次        
-            if((new Date(Date.now()).getTime() - new Date().getTime()) == 0) {  // 目前登入時間未超過今日12:00
-              this.setState({ failTimes: this.state.failTimes + 1 }, () => {window.localStorage.setItem("EMIPD_FAIL", JSON.stringify(newRecord));} )
-            }else{  // 如果跨日，就重新寫入全部的值、次數調為1
+          } else {
+            // 登入錯誤次數2~4次
+            if (new Date(Date.now()).getTime() - new Date().getTime() == 0) {
+              // 目前登入時間未超過今日12:00
+              this.setState({ failTimes: this.state.failTimes + 1 }, () => {
+                window.localStorage.setItem(
+                  "EMIPD_FAIL",
+                  JSON.stringify(newRecord)
+                );
+              });
+            } else {
+              // 如果跨日，就重新寫入全部的值、次數調為1
               newRecord = {
                 ...oldRecord,
-                count: 1
-              }
-              this.setState({ failTimes: 1 }, () => {window.localStorage.setItem("EMIPD_FAIL", JSON.stringify(newRecord));} )
+                count: 1,
+              };
+              this.setState({ failTimes: 1 }, () => {
+                window.localStorage.setItem(
+                  "EMIPD_FAIL",
+                  JSON.stringify(newRecord)
+                );
+              });
             }
           }
         }
@@ -138,28 +185,30 @@ class LoginScreen extends React.Component {
       this.setState({
         isLoading: false,
       });
-    }
+    };
 
-    this.setState({
-      isLoading: true,
-    }, () => {
-      login(value, callback, errorCallback);
-    });
+    this.setState(
+      {
+        isLoading: true,
+      },
+      () => {
+        login(value, callback, errorCallback);
+      }
+    );
   };
 
-  
   handleResetPasswordModal = () => {
     this.setState({
       resetPasswordModalVisible: true,
-    })
-  }
+    });
+  };
 
   closeModal = () => {
     this.setState({
-      isLoading: false, 
+      isLoading: false,
       resetPasswordModalVisible: false,
-    })
-  }
+    });
+  };
 
   handleChangePwd = (value) => {
     const { changePassword } = this.props;
@@ -168,28 +217,31 @@ class LoginScreen extends React.Component {
     const callback = () => {
       this.setState({
         isLoading: false,
-        resetPasswordModalVisible: false
-      })
+        resetPasswordModalVisible: false,
+      });
     };
 
     const errorCallback = (value) => {
-          this.setState({
-            isLoading: false,
-          })
-        }
+      this.setState({
+        isLoading: false,
+      });
+    };
 
     let newData = {
       user_id: firstLoginUser.user_id,
-      old_password: '0000',
-      new_password: value.new_password
-    }
+      old_password: "0000",
+      new_password: value.new_password,
+    };
 
-    this.setState({
-      isLoading: true,
-    }, () => {
-      changePassword(newData, callback, errorCallback, firstLoginToken);
-    })
-  }
+    this.setState(
+      {
+        isLoading: true,
+      },
+      () => {
+        changePassword(newData, callback, errorCallback, firstLoginToken);
+      }
+    );
+  };
 
   renderResetPasswordModal = () => {
     const { resetPasswordModalVisible, firstLoginUser } = this.state;
@@ -205,12 +257,18 @@ class LoginScreen extends React.Component {
         onCancel={() => this.closeModal()}
         footer={null}
       >
-        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
           <Form
-            name='basic'
+            name="basic"
             initialValues={firstLoginUser}
             onFinish={this.handleChangePwd}
-            style={{ width: '100%' }}
+            style={{ width: "100%" }}
           >
             <Form.Item
               name="new_password"
@@ -218,10 +276,33 @@ class LoginScreen extends React.Component {
               label="新密碼"
               labelCol={{ span: 24 }}
               wrapperCol={{ span: 24 }}
-              rules={[{ required: true, message: '請輸入新密碼' }]}
+              rules={[
+                { required: true, message: "請輸入新密碼" },
+                {
+                  validator: function (_, value) {
+                    if (!value) {
+                      return Promise.reject("請輸入新密碼");
+                    }
+
+                    if (value.length < 10) {
+                      return Promise.reject("密碼長度需至少 10 碼");
+                    }
+
+                    const hasUppercase = /[A-Z]/.test(value);
+                    const hasLowercase = /[a-z]/.test(value);
+                    const hasNumber = /[0-9]/.test(value);
+
+                    if (!hasUppercase || !hasLowercase || !hasNumber) {
+                      return Promise.reject("密碼需包含英文大小寫與數字");
+                    }
+
+                    return Promise.resolve();
+                  },
+                },
+              ]}
             >
               <Input.Password
-                placeholder='請輸入新密碼'
+                placeholder="請輸入新密碼"
                 style={styles.inputStyle}
               />
             </Form.Item>
@@ -233,47 +314,65 @@ class LoginScreen extends React.Component {
               labelCol={{ span: 24 }}
               wrapperCol={{ span: 24 }}
               required
-              rules={[{ required: true, message: '此欄位不可為空' }]}
+              rules={[{ required: true, message: "此欄位不可為空" }]}
               // dependencies={['new_password']}
               rules={[
-                ({ getFieldValue }) =>
-                ({
+                ({ getFieldValue }) => ({
                   validator(_, value) {
-                    if (getFieldValue('new_password') != value && value == null && value == '') {
-                      return Promise.reject(new Error('此欄位不可為空！'));
-                    } else if (!value || getFieldValue('new_password') === value) {
+                    if (
+                      getFieldValue("new_password") != value &&
+                      value == null &&
+                      value == ""
+                    ) {
+                      return Promise.reject(new Error("此欄位不可為空！"));
+                    } else if (
+                      !value ||
+                      getFieldValue("new_password") === value
+                    ) {
                       return Promise.resolve();
                     }
-                    return Promise.reject(new Error('與新密碼不一致'));
+                    return Promise.reject(new Error("與新密碼不一致"));
                   },
                 }),
               ]}
             >
               <Input.Password
-                placeholder='請輸入確認密碼'
+                placeholder="請輸入確認密碼"
                 style={styles.inputStyle}
               />
             </Form.Item>
-            <div style={{ width: '100%', display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-              <Button style={styles.btnStyle} htmlType='submit'>確定</Button>
+            <div
+              style={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
+                marginTop: "20px",
+              }}
+            >
+              <Button style={styles.btnStyle} htmlType="submit">
+                確定
+              </Button>
             </div>
           </Form>
         </div>
       </Modal>
-    )
-  }
+    );
+  };
 
   render() {
     const { isLoading, failTimes, resetPasswordModalVisible } = this.state;
     return (
-      <div style={{ width: '100%', height: '100%' }}>
-        <LoginMiddleView isLoading={isLoading} handleLogin={this.handleLogin} failTimes={failTimes} />
+      <div style={{ width: "100%", height: "100%" }}>
+        <LoginMiddleView
+          isLoading={isLoading}
+          handleLogin={this.handleLogin}
+          failTimes={failTimes}
+        />
         {resetPasswordModalVisible && this.renderResetPasswordModal()}
       </div>
     );
   }
 }
-
 
 export default connect(
   (state) => ({
@@ -286,6 +385,6 @@ export default connect(
         login: UserActions.login,
         changePassword: UserActions.changePassword,
       },
-      dispatch,
-    ),
+      dispatch
+    )
 )(LoginScreen);
